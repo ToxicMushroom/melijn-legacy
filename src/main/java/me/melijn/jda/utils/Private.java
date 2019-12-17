68aac81d34c8e7e9c391770caa637e58ac880cf6
Merlijn Verstraete <32853531+ToxicMushroom@users.noreply.github.com>
2019-12-17 14:17:47 +0100

    added needed files

---
+++ src/main/java/me/melijn/jda/utils/Private.java
@@ -0,0 +1,10 @@
+package me.melijn.jda.utils;
+
+public class Private {
+
+    public String getWeebV1Url(WebUtils webUtils, String type) {
+        //Cannot share source of this
+        //TODO: Fix
+        return "error";
+    }
+}
